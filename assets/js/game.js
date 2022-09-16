96ec1d3e7e3db83597a0abbc9ce471a9abf7cdac
Shawn Willard <shawnmwillard@gmail.com>
2022-09-15 20:38:56 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -85,7 +85,14 @@ let fight = function (enemyName) {
 }; // end of fight function
 
 for (let i = 0; i < enemyNames.length; i++) {
+  if (playerHealth > 0) {
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+  } else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
   let pickedEnemyName = enemyNames[i];
   enemyHealth = 50;
+  debugger;
   fight(pickedEnemyName);
 }
